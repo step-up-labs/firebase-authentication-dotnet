5ca6b2da88957b440b31fe9d2547bed9ab71dacf
itswisdomagain <wisdom.arerosuoghene@gmail.com>
2016-12-31 10:49:38 +0100

    Updated AuthErrorReasons to better interprete more error messages from the Google Identity Toolkit API

--- src/Firebase.Auth/AuthErrorReason.cs
+++ src/Firebase.Auth/AuthErrorReason.cs
@@ -7,21 +7,73 @@
         /// </summary>
         Undefined,
         /// <summary>
-        /// No user with a matching email address is registered.
+        /// The user was disabled and is not granted access anymore.
         /// </summary>
-        UnknownEmailAddress,
+        UserDisabled,
         /// <summary>
-        /// The supplied ID is not a valid email address.
+        /// Third-party Auth Providers: PostBody does not contain or contains invalid Authentication Provider string.
+        /// </summary>
+        InvalidProviderID,
+        /// <summary>
+        /// Third-party Auth Providers: PostBody does not contain or contains invalid Access Token string obtained from Auth Provider.
+        /// </summary>
+        InvalidAccessToken,
+        /// <summary>
+        /// Changes to user's account has been made since last log in. User needs to login again.
+        /// </summary>
+        LoginCredentialsTooOld,
+        /// <summary>
+        /// Third-party Auth Providers: Request does not contain a value for parameter: requestUri.
+        /// </summary>
+        MissingRequestURI,
+        /// <summary>
+        /// Third-party Auth Providers: Request does not contain a value for parameter: postBody.
+        /// </summary>
+        SystemError,
+        /// <summary>
+        /// Email/Password Authentication: Email address is not in valid format.
         /// </summary>
         InvalidEmailAddress,
         /// <summary>
-        /// Wrong password.
+        /// Email/Password Authentication: No password provided!
+        /// </summary>
+        MissingPassword,
+        /// <summary>
+        /// Email/Password Signup: Password must be more than 6 characters.  This error could also be caused by attempting to create a user account using Set Account Info without supplying a password for the new user.
+        /// </summary>
+        WeakPassword,
+        /// <summary>
+        /// Email/Password Signup: Email address already connected to another account. This error could also be caused by attempting to create a user account using Set Account Info and an email address already linked to another account.
+        /// </summary>
+        EmailExists,
+        /// <summary>
+        /// Email/Password Signin: No email provided! This error could also be caused by attempting to create a user account using Set Account Info without supplying an email for the new user.
+        /// </summary>
+        MissingEmail,
+        /// <summary>
+        /// Email/Password Signin: No user with a matching email address is registered.
+        /// </summary>
+        UnknownEmailAddress,
+        /// <summary>
+        /// Email/Password Signin: The supplied password is not valid for the email address.
         /// </summary>
         WrongPassword,
         /// <summary>
-        /// The user was disabled and is not granted access anymore.
+        /// Password Recovery: Request does not contain a value for parameter: requestType or supplied value is invalid.
         /// </summary>
-        UserDisabled
+        MissingRequestType,
+        /// <summary>
+        /// Account Linking: Authenticated User ID Token is invalid!
+        /// </summary>
+        InvalidIDToken,
+        /// <summary>
+        /// Linked Accounts: Request does not contain a value for parameter: identifier.
+        /// </summary>
+        MissingIdentifier,
+        /// <summary>
+        /// Linked Accounts: Request contains an invalid value for parameter: identifier.
+        /// </summary>
+        InvalidIdentifier
 
     }
 }
