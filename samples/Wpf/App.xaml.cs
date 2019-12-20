63c3b4739b2f3a04d21dc920828754cc4e9db3c6
bezysoftware <bezysoftware@hotmail.cz>
2019-12-20 15:04:35 +0100

    WPF sample

---
+++ samples/Wpf/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Firebase.Auth.Providers;
+using Firebase.Auth.UI;
+using System.Globalization;
+using System.Windows;
+
+namespace Firebase.Auth.Wpf.Sample
+{
+    public partial class App : Application
+    {
+        public App()
+        {    
+            // Force override culture & language
+            //CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("cs");
+            //CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("cs");
+
+            // Firebase UI initialization
+            FirebaseUI.Initialize(new FirebaseUIConfig
+            {
+                ApiKey = "<YOUR API KEY>",
+                AuthDomain = "<YOUR PROJECT DOMAIN>.firebaseapp.com",
+                Providers = new FirebaseAuthProvider[]
+                {
+                    new GoogleProvider(),
+                    new FacebookProvider(),
+                    new TwitterProvider(),
+                    new GithubProvider(),
+                    new MicrosoftProvider(),
+                    new EmailProvider()
+                },
+                PrivacyPolicyUrl = "https://github.com/step-up-labs/firebase-authentication-dotnet",
+                TermsOfServiceUrl = "https://github.com/step-up-labs/firebase-database-dotnet",
+                IsAnonymousAllowed = true
+            });
+        }
+    }
+}
