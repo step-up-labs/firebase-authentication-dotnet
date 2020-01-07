a6ca6882964e7e24cc1c354c824b0df340818ea3
bezysoftware <bezysoftware@outlook.com>
2020-01-07 23:37:06 +0100

    Unit and integration tests

---
+++ tests/Auth.Tests/ErrorParserTests.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Firebase.Auth.Tests
+{
+    public class ErrorParserTests
+    {
+        public static IEnumerable<object[]> Data =>
+            new List<object[]>
+            {
+                new object[] {
+                    @"{
+                        ""error"": {
+                        ""code"": 400,
+                        ""message"": ""WEAK_PASSWORD : Password should be at least 6 characters"",
+                        ""errors"": [
+                            {
+                            ""message"": ""WEAK_PASSWORD : Password should be at least 6 characters"",
+                            ""domain"": ""global"",
+                            ""reason"": ""invalid""
+                            }
+                        ]
+                        }
+                    }", 
+                    AuthErrorReason.WeakPassword },
+                new object[] {
+                    @"{
+                        ""error"": {
+                        ""code"": 400,
+                        ""message"": ""INVALID_PASSWORD"",
+                        ""errors"": [
+                            {
+                            ""message"": ""INVALID_PASSWORD"",
+                            ""domain"": ""global"",
+                            ""reason"": ""invalid""
+                            }
+                        ]
+                        }
+                    }",
+                    AuthErrorReason.WrongPassword },
+
+            };
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void Test(string response, AuthErrorReason error)
+        {
+            var result = FirebaseFailureParser.GetFailureReason(response);
+
+            result.Should().Be(error);
+        }
+    }
+}
---
+++ tests/Auth.Tests/Integration/EmailTests.cs
@@ -0,0 +1,122 @@
+﻿using Firebase.Auth;
+using Firebase.Auth.Providers;
+using FluentAssertions;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Firebase.Auth.Tests.Integration
+{
+    [Trait("Category", "Integration")]
+    public class EmailTests
+    {
+        private const string ValidPassword = "super_secret_password";
+        private const string InvalidPassword = "short";
+        private const string NewUserEmail = "newintegration@test.com"; 
+        private const string DisplayName = "Test User"; 
+
+        private readonly FirebaseAuthClient client;
+        
+        public EmailTests()
+        {
+            var setup = Setup.LoadFromEnvironment();
+            this.client = new FirebaseAuthClient(new FirebaseAuthConfig
+            {
+                ApiKey = setup.ApiKey,
+                AuthDomain = setup.AuthDomain,
+                Providers = new FirebaseAuthProvider[]
+                {
+                    new EmailProvider()
+                }
+            });
+        }
+
+        [Fact]
+        public async Task CreateUserTest()
+        {
+            var user = await this.client.CreateUserWithEmailAndPasswordAsync(NewUserEmail, ValidPassword, DisplayName);
+
+            try
+            {
+                user.Uid.Should().NotBeNullOrEmpty();
+                user.Info.Email.Should().Be(NewUserEmail);
+                user.Info.DisplayName.Should().Be(DisplayName);
+            }
+            finally
+            {
+                await user.DeleteAsync();
+            }
+        }
+
+        [Fact]
+        public async Task SignInTest()
+        {
+            var originalUser = await this.client.CreateUserWithEmailAndPasswordAsync(NewUserEmail, ValidPassword, DisplayName);
+            
+            try
+            {
+                var user = await this.client.SignInWithEmailAndPasswordAsync(NewUserEmail, ValidPassword);
+
+                user.Uid.Should().NotBeNullOrEmpty();
+                user.Info.Email.Should().Be(NewUserEmail);
+                user.Info.DisplayName.Should().Be(DisplayName);
+            }
+            finally
+            {
+                await originalUser.DeleteAsync();
+            }
+        }
+
+        [Fact]
+        public async Task SignInWithCredentialTest()
+        {
+            var originalUser = await this.client.CreateUserWithEmailAndPasswordAsync(NewUserEmail, ValidPassword, DisplayName);
+
+            try
+            {
+                var credential = EmailProvider.GetCredential(NewUserEmail, ValidPassword);
+                var user = await this.client.SignInWithCredentialAsync(credential);
+
+                user.Uid.Should().NotBeNullOrEmpty();
+                user.Info.Email.Should().Be(NewUserEmail);
+                user.Info.DisplayName.Should().Be(DisplayName);
+            }
+            finally
+            {
+                await originalUser.DeleteAsync();
+            }
+        }
+
+        [Fact]
+        public async Task SignInInvalidPasswordTest()
+        {
+            var user = await this.client.CreateUserWithEmailAndPasswordAsync(NewUserEmail, ValidPassword, DisplayName);
+
+            try
+            {
+                Func<Task<User>> signIn = () => this.client.SignInWithEmailAndPasswordAsync(NewUserEmail, InvalidPassword);
+                await signIn.Should().ThrowAsync<FirebaseAuthException>();
+            }
+            finally
+            {
+                await user.DeleteAsync();
+            }
+        }
+
+        [Fact]
+        public async Task ResetPasswordTest()
+        {
+            var user = await this.client.CreateUserWithEmailAndPasswordAsync(NewUserEmail, ValidPassword, DisplayName);
+
+            try
+            {
+                // only check the call succeeds
+                await client.ResetEmailPasswordAsync(NewUserEmail);
+            }
+            finally
+            {
+                await user.DeleteAsync();
+            }
+        }
+    }
+}
