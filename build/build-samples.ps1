0f43932ae958c271c88bff9cf072b108cf329b23
bezysoftware <bezysoftware@outlook.com>
2020-02-18 11:19:55 +0100

    Fix UWP build

---
+++ build/build-samples.ps1
@@ -0,0 +1,3 @@
+﻿dotnet build --configuration release .\samples\Console\Auth.Console.Sample.csproj
+dotnet build --configuration release .\samples\WPF\Auth.WPF.Sample.csproj
+msbuild /p:Configuration=Debug .\samples\UWP\Auth.UWP.Sample.csproj 
