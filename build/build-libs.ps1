a1be9655643a3a3bf5356916651ede91e98d698e
bezysoftware <bezysoftware@outlook.com>
2020-02-18 15:08:19 +0100

    Restore UWP packages before build

--- build/build-libs.ps1
+++ build/build-libs.ps1
@@ -1,4 +1,5 @@
 ﻿dotnet build --configuration release .\src\Auth\Auth.csproj
 dotnet build --configuration release .\src\Auth.UI\Auth.UI.csproj
 dotnet build --configuration release .\src\Auth.UI.WPF\Auth.UI.WPF.csproj
+msbuild /t:restore .\src\Auth.UI.UWP\Auth.UI.UWP.csproj
 msbuild /p:Configuration=Release .\src\Auth.UI.UWP\Auth.UI.UWP.csproj 
--- build/build-samples.ps1
+++ build/build-samples.ps1
@@ -1,3 +1,4 @@
 ﻿dotnet build --configuration release .\samples\Console\Auth.Console.Sample.csproj
 dotnet build --configuration release .\samples\WPF\Auth.WPF.Sample.csproj
+msbuild /t:restore .\samples\UWP\Auth.UWP.Sample.csproj 
 msbuild /p:Configuration=Debug .\samples\UWP\Auth.UWP.Sample.csproj 
