a60bc7c48ae10942543e551fccef7cbc90b4dd8c
bezysoftware <bezysoftware@outlook.com>
2020-02-21 09:06:27 +0100

    Restore UWP project first

--- build/run-pack.ps1
+++ build/run-pack.ps1
@@ -7,11 +7,11 @@ if ($preview) {
 	write "Creating preview packages with version suffix $suffix"
 	dotnet pack --configuration release --output $output -p:version-suffix=$suffix .\src\Auth\Auth.csproj
 	dotnet pack --configuration release --output $output -p:version-suffix=$suffix .\src\Auth.UI.WPF\Auth.UI.WPF.csproj
-	msbuild /t:pack /p:Configuration=Release /p:PackageOutputPath=$output /p:VersionSuffix=$suffix .\src\Auth.UI.UWP\Auth.UI.UWP.csproj 
+	msbuild /t:restore,pack /p:Configuration=Release /p:PackageOutputPath=$output /p:VersionSuffix=$suffix .\src\Auth.UI.UWP\Auth.UI.UWP.csproj 
 } else {
 	$version = $(git describe --tags --abbrev=0).substring(1)
 	write "Creating packages with tag version $version"
 	dotnet pack --configuration release --output $output -p:version=$version .\src\Auth\Auth.csproj
 	dotnet pack --configuration release --output $output -p:version=$version .\src\Auth.UI.WPF\Auth.UI.WPF.csproj
-	msbuild /t:pack /p:Configuration=Release /p:PackageOutputPath=$output /p:Version=$version .\src\Auth.UI.UWP\Auth.UI.UWP.csproj 
+	msbuild /t:restore,pack /p:Configuration=Release /p:PackageOutputPath=$output /p:Version=$version .\src\Auth.UI.UWP\Auth.UI.UWP.csproj 
 }
