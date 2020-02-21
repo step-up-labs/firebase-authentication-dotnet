ee57f3c4ef7603eccc6399b961552626d9738957
bezysoftware <bezysoftware@outlook.com>
2020-02-21 09:41:45 +0100

    Fix pack version suffix

--- build/run-pack.ps1
+++ build/run-pack.ps1
@@ -5,8 +5,8 @@ $output = Join-Path $pwd "artifacts"
 if ($preview) {
 	$suffix = "preview-$(Get-Date -format yyyyMMddHHmmss)"
 	write "Creating preview packages with version suffix $suffix"
-	dotnet pack --configuration release --output $output -p:version-suffix=$suffix .\src\Auth\Auth.csproj
-	dotnet pack --configuration release --output $output -p:version-suffix=$suffix .\src\Auth.UI.WPF\Auth.UI.WPF.csproj
+	dotnet pack --configuration release --output $output --version-suffix=$suffix .\src\Auth\Auth.csproj
+	dotnet pack --configuration release --output $output --version-suffix=$suffix .\src\Auth.UI.WPF\Auth.UI.WPF.csproj
 	msbuild /t:restore,pack /p:Configuration=Release /p:PackageOutputPath=$output /p:VersionSuffix=$suffix .\src\Auth.UI.UWP\Auth.UI.UWP.csproj 
 } else {
 	$version = $(git describe --tags --abbrev=0).substring(1)
