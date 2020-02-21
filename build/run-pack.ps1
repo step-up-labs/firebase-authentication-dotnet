ec8e288dbeb9acddb551a4667f811a16a2dd556d
bezysoftware <bezysoftware@outlook.com>
2020-02-21 08:52:13 +0100

    Omit --no-build for now

--- build/run-pack.ps1
+++ build/run-pack.ps1
@@ -3,10 +3,10 @@
 $output = Join-Path $pwd "artifacts"
 
 if ($preview) {
-	$suffix = "preview-$(date -format yyyyMMddHHmmss)"
+	$suffix = "preview-$(Get-Date -format yyyyMMddHHmmss)"
 	write "Creating preview packages with version suffix $suffix"
-	dotnet pack --configuration release --output $output -p:version=$suffix .\src\Auth\Auth.csproj
-	dotnet pack --configuration release --output $output -p:version=$suffix .\src\Auth.UI.WPF\Auth.UI.WPF.csproj
+	dotnet pack --configuration release --output $output -p:version-suffix=$suffix .\src\Auth\Auth.csproj
+	dotnet pack --configuration release --output $output -p:version-suffix=$suffix .\src\Auth.UI.WPF\Auth.UI.WPF.csproj
 	msbuild /t:pack /p:Configuration=Release /p:PackageOutputPath=$output /p:VersionSuffix=$suffix .\src\Auth.UI.UWP\Auth.UI.UWP.csproj 
 } else {
 	$version = $(git describe --tags --abbrev=0).substring(1)
