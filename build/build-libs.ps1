a0061bc22ac08450e7bf4fcb60a3783fa6c12244
Tomas Bezouska <bezysoftware@outlook.com>
2020-02-21 09:14:47 +0100

    Build of FirebasUI fpr UWP
    * Fix UWP build
    
    * Ignore UWP build artifacts
    
    * Refactor github yaml
    
    * Update dotnetcore.yml
    
    * Restore UWP packages before build
    
    * Restructured build process to better handle UWP
    
    * Fix build workflow
    
    * Test pack step
    
    * Omit --no-build for now
    
    * Checkout in pack
    
    * Restore UWP project first

---
+++ build/build-libs.ps1
@@ -0,0 +1,4 @@
+﻿dotnet build --configuration release .\src\Auth\Auth.csproj
+dotnet build --configuration release .\src\Auth.UI\Auth.UI.csproj
+dotnet build --configuration release .\src\Auth.UI.WPF\Auth.UI.WPF.csproj
+msbuild /restore /p:Configuration=Release .\src\Auth.UI.UWP\Auth.UI.UWP.csproj 
