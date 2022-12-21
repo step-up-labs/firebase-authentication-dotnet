bd8eb60bbaf4b172bb2ca49f404075c3faf627d0
DerKO <dr.knockout99@gmail.com>
2022-12-20 18:39:42 -0700

    Adds support for auth emulator

--- src/Firebase.Auth/FirebaseConfig.cs
+++ src/Firebase.Auth/FirebaseConfig.cs
@@ -9,15 +9,37 @@
         /// Initializes a new instance of the <see cref="FirebaseConfig"/> class.
         /// </summary>
         /// <param name="apiKey"> The api key of your Firebase app. </param>
-        public FirebaseConfig(string apiKey)
+        /// <param name="usesEmulator"> If the emulator is being used. Default is false. </param>
+        /// <param name="emulatorPort"> What port the emulator is using. Default is 9099. </param>
+        public FirebaseConfig(string apiKey, bool usesEmulator = false, int emulatorPort = 9099)
         {
             this.ApiKey = apiKey;
+            this.UsesEmulator = usesEmulator;
+            this.EmulatorPort = emulatorPort;
         }
 
         /// <summary>
         /// Gets or sets the api key of your Firebase app. 
         /// </summary>
-        public string ApiKey 
+        public string ApiKey
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets whether the emulator is used or not. 
+        /// </summary>
+        public bool UsesEmulator
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the port the emulator uses. 
+        /// </summary>
+        public int EmulatorPort
         {
             get;
             set;
